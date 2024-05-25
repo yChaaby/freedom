56ff41c2f3aa9a7b71d339d99aed9d261d4303f8
yChaaby <113364055+yChaaby@users.noreply.github.com>
2024-05-25 14:09:12 +0200

    fix some bugs

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -18,13 +18,13 @@ public class Client {
     public UserRemote stub;
     public static void main(String[] args)  {
         try {
-            User thisuser = new User("mazigh1",new Date("21/10/2001"));
-            Client client = new Client(thisuser);
+            Client client = new Client();
             client.connect();
             OpinionTopic c = new OpinionTopic(client.user,new Topic("is the Raja CA the best in the world ?"),1);
             client.user.addOpinion(c);
             client.user.displayOpinions();
-            client.sendOpinionTo(c,"koceila1");
+            //client.sendOpinionTo(c,"koceila1");
+
             //ClientMonitor receiver = (ClientMonitor) client.stub.getClientMonitor("koceila1");
             //receiver.displayMessage("Dima Dima RAJA");
 
@@ -38,14 +38,31 @@ public class Client {
         ClientMonitor receiver = (ClientMonitor) this.stub.getClientMonitor(username);
         receiver.sendOpinion(op);
     }
-    public Client(User user) throws RemoteException {
-        this.user = user;
+    public void addOpinion(){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter opinion (ex: 0.5) / 1) : ");
+        float opinion = scanner.nextFloat();
+
+
+    }
+    public Client() throws RemoteException {
+        String username;
+        Date birthday;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your username: ");
+        username = scanner.nextLine();
+        System.out.print("Enter your birthday (format DD/MM/YYYY): ");
+        birthday = new Date(scanner.nextLine());
+        this.user = new User(username,birthday);
         this.monitor = new Monitor(user);
     }
+
     public void connect() throws RemoteException, NotBoundException {
         Registry registry = LocateRegistry.getRegistry("127.0.0.1",12345);
         this.stub = (UserRemote) registry.lookup("Server");
         stub.addListener(monitor);
     }
+
 
 }
